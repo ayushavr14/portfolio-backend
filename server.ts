d31cfab2905ea8c255f54b1127bc055820ace771
Ayushavr14 <ayushavr007@gmail.com>
2024-09-02 16:48:43 +0530

    fix: add frontend url

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ connectDB();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://illustrious-hummingbird-5b31aa.netlify.app/",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
